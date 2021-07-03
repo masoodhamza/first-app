f02d2c19ddfc32a38279bc4837e9c06766734461
Hamza Masood <masood.hamzaa@gmail.com>
2021-07-03 14:39:21 +0500

    data feteching by button

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -4,22 +4,25 @@ import { useEffect, useState } from "react";
 const DataFetching = () => {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
 
   useEffect(() => {
     axios
-      .get(`http://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`http://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         console.log(res.data);
         setPost(res.data);
       })
       .catch((error) => console.log(error));
-  }, [id]);
+  }, [idFromButtonClick]);
 
   return (
     <>
       <h1>Posts</h1>
       <input type="number" value={id} onChange={(e) => setId(e.target.value)} />
-
+      <button type="button" onClick={() => setIdFromButtonClick(id)}>
+        Fetch data
+      </button>
       <div key={post.id}>
         <h2>{post.title}</h2>
         <p>{post.body}</p>
