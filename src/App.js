cd66b2b3d45c062f90a02c061e73b58313b74d2f
Hamza Masood <masood.hamzaa@gmail.com>
2021-07-03 11:30:57 +0500

    useState with array

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import HookUseState from "./components/HookUseState";
 import HookPrvState from "./components/HookPrvState";
 import ObjUseState from "./components/ObjUseState";
+import ArrayUseState from "./components/ArrayUseState";
 
 const App = () => {
   return (
     <div className="App">
       {/* <HookUseState /> */}
       {/* <HookPrvState /> */}
-      <ObjUseState />
+      {/* <ObjUseState /> */}
+      <ArrayUseState />
     </div>
   );
 };
