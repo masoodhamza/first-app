60d5a11f868311ba416f6a3de53f0a38fede1d32
Hamza Masood <masood.hamzaa@gmail.com>
2021-07-03 12:23:53 +0500

    conditnal effect

--- src/components/HookUseEffect.js
+++ src/components/HookUseEffect.js
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react";
 
 const HookUseEffect = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
   useEffect(() => {
+    console.log("state updating");
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         Click {count} times
       </button>
