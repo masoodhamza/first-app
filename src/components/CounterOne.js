6e33f84a31d62b5fd8597792b9eec94d8ad47614
Hamza Masood <masood.hamzaa@gmail.com>
2021-07-04 17:12:52 +0500

    simeple useReducer

---
+++ src/components/CounterOne.js
@@ -0,0 +1,30 @@
+import { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const CounterOne = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <h2>count {count}</h2>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
+      <button onClick={() => dispatch("reset")}>Reset</button>
+    </>
+  );
+};
+
+export default CounterOne;
